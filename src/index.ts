ad4a6d0c26aa7d2b7fbe0a80c49b4a0bd2ea6ad6
Helder S Ribeiro <hsribei.pub@gmail.com>
2018-01-22 17:50:50 -0300

    first working world map view

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
 import "./style.css";
+import * as d3 from "d3";
+import * as topojson from "topojson-client";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const context = d3
+      .select("canvas")
+      .node()
+      .getContext("2d"),
+    path = d3.geoPath(d3.geoOrthographic(), context);
+
+  d3.json("https://unpkg.com/world-atlas@1/world/110m.json", (error, world) => {
+    if (error) throw error;
+
+    context.beginPath();
+    path(topojson.mesh(world));
+    context.stroke();
+  });
+});
