480e83e4da5b0c1852791e940cd8edb82916bd49
Helder S Ribeiro <hsribei.pub@gmail.com>
2018-01-23 17:43:24 -0300

    Change to Mercator projection

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import * as d3 from "d3";
 import * as topojson from "topojson-client";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const projection = d3.geoMercator();
   const context = d3
       .select("canvas")
       .node()
       .getContext("2d"),
-    path = d3.geoPath(d3.geoOrthographic(), context);
+    path = d3.geoPath(projection, context);
 
   d3.json("https://unpkg.com/world-atlas@1/world/110m.json", (error, world) => {
     if (error) throw error;
